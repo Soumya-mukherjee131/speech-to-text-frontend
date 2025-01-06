9727044d92266d59e95c253f59bbf855fd7979cc
Soumyadip Mukherjee <139647504+Soumya-mukherjee131@users.noreply.github.com>
2025-01-06 20:30:49 +0530

    Added new key

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     formData.append('file', file);
 
     try {
-      const response = await axios.post('https://speech-to-text-backend.onrender.com/process-audio', formData, {
+      const response = await axios.post('https://speech-to-text-backend-2.onrender.com/process-audio', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         },
